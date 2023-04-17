13a0d5b0e60a620569c5a27204ed518e13708e5d
Eph Baum <fskirschbaum@gmail.com>
2023-04-16 23:20:42 -0700

    perhaps this is the value when i look at pow site

--- config/config.exs
+++ config/config.exs
@@ -67,7 +67,7 @@ config :elxrBB, :pow,
   extensions: [PowResetPassword, PowEmailConfirmation],
   controller_callbacks: Pow.Extension.Phoenix.ControllerCallbacks,
   mailer_backend: ElxrBB.Pow.Mailer,
-  identity_field: :username
+  user_id_field: :username
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{config_env()}.exs"
